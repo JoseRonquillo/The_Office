250c8abe30cde0242053a7c0da8df586c2db0702
JoseRonquillo <114964339+JoseRonquillo@users.noreply.github.com>
2023-02-14 09:38:42 -0600

    Agregue un gitignore y modifique el main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,15 @@
 print("Jose Antonio Ronquillo Meza")
 print("Estudiante de ingenieria en sistemas")
 print("Carnet: 1547223")
-Print("Universidad rafael landivar")
+print("Universidad rafael landivar")
+
+import os
+
+mi_ubicacion = os.getcwd()
+if os.path.exists("modulos"):
+    print("La carpeta ya existe")
+else:
+    os.mkdir(mi_ubicacion + "\\modulos")
+    archivo = open('./modulos/prueba.txt', 'w')
+    archivo.write('Hola mundo')
+    archivo.close()
